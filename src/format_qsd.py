2b9274c0e114f9b9e4258f739ea6fa609c23ff97
BenjaminHuefner <benjaminh61505@gmail.com>
2026-07-17 19:08:19 +0000

    fixed issue of truncated input

--- src/format_qsd.py
+++ src/format_qsd.py
@@ -1,9 +1,26 @@
 import struct
 import numpy as np
-from matplotlib import pyplot as plt # for demo only: remove otherwise
 import pandas as pd
 
 from src.analyze import ordinal
+
+def _safe_unpack_doubles(d, pointer, n):
+    """Unpack up to n little-endian doubles starting at pointer.
+
+    Some files have a channel whose recording was
+    cut short, so the array on disk is shorter than the length declared
+    elsewhere in the file. Rather than crashing with a struct.error, read
+    as many doubles as are actually available and pad the remainder with
+    zeros -- consistent with how mismatched overtone lengths are already
+    handled elsewhere in this parser.
+    """
+    available_bytes = max(0, len(d) - pointer)
+    available_n = available_bytes // 8
+    read_n = min(n, available_n)
+    val = np.array(struct.unpack('<{}d'.format(read_n), d[pointer:pointer + read_n * 8]))
+    if read_n < n:
+        val = np.append(val, np.zeros(n - read_n))
+    return val
 
 def read_qsd(filename):
     with open(filename, 'rb') as f:
@@ -42,32 +59,42 @@ def read_qsd(filename):
         no_pad = False
     else:
         no_pad = False
-    
+
+    pointer += 4     # skip length information
+    if d[pointer] == 0x02:
+        pointer += 8 # added to validate BSA dataset
+    if d[pointer] != 0x01:
+        raise Exception("Invalid value: != 0x01")
+    pointer += 12
+    if d[pointer] != 0x0b:
+        raise Exception("Invalid value: != 0x0b")
+
     pointer += 6
-    val = struct.unpack('<{}d'.format(n), d[pointer:pointer+n*8])
-    val = np.array(val)
+    val = _safe_unpack_doubles(d, pointer, n)
     tim = (val - val[0])*86400;
     
     pointer += n*8-1+3 if no_pad else n*8-1+8*1+3
     n = struct.unpack('<I', d[pointer:pointer+4])[0]
     reslen.append(n)
     pointer += 4
-    val = struct.unpack('<{}d'.format(n), d[pointer:pointer+n*8])
-    fre = np.array(val)
+    fre = _safe_unpack_doubles(d, pointer, n)
     
     pointer += n*8-1
     pointer += 7
-    val = struct.unpack('<{}d'.format(n), d[pointer:pointer+n*8])
-    dis = np.array(val)
+    dis = _safe_unpack_doubles(d, pointer, n)
     
     pointer += n*8-1
     while True:
         pointer += 9
+        if pointer + 4 > len(d):
+            break  # ran out of file before the next block header
         n = struct.unpack('<I', d[pointer:pointer+4])[0]
         
         if n == 0:
             nsensors -= 1
             pointer += 40
+            if pointer + 4 > len(d):
+                break
             n = struct.unpack('<I', d[pointer:pointer+4])[0]
             
             if nsensors == 0:
@@ -76,7 +103,7 @@ def read_qsd(filename):
         reslen.append(n)
         pointer -= 2
         pointer += 3*8
-        val = np.array(struct.unpack('<{}d'.format(n), d[pointer:pointer+n*8]))
+        val = _safe_unpack_doubles(d, pointer, n)
         timtmp = (val - val[0]) * 86400 
         if (len(timtmp)<max(np.shape((tim)))):
             timtmp=np.append(timtmp,0)
@@ -86,18 +113,19 @@ def read_qsd(filename):
         
         pointer += n*8-1
         pointer += 3
+        if pointer + 4 > len(d):
+            break
         n = struct.unpack('<I', d[pointer:pointer+4])[0]
         
         pointer += 4
-        val = struct.unpack('<{}d'.format(n), d[pointer:pointer+n*8])
-        val = np.array(val)
+        val = _safe_unpack_doubles(d, pointer, n)
         if (len(val)<max(np.shape((fre)))):
             val=np.append(val,0)
         fre=np.vstack([fre,val])        
         
         pointer += n*8-1
         pointer += 7
-        val = struct.unpack('<{}d'.format(n), d[pointer:pointer+n*8])
+        val = _safe_unpack_doubles(d, pointer, n)
         if (len(val)<max(np.shape((dis)))):
             val=np.append(val,0)
         dis=np.vstack([dis,val])        
@@ -106,55 +134,26 @@ def read_qsd(filename):
     return tim, fre, dis, reslen, ns
 
 def extract_sensor_data(time,freq,dis,reslen,ns):
-    """take the formatted raw qsd file and put it into a dataframe, into a csv for later use
-    time, freq, dis are 2d arrs as follows:
-    arr -> [ [overtone1 sensor1], [overtone2 sensor1], ... ,
-    [overtone_n sensor1], [overtone1 sensor2], [overtone2 sensor2], ... , [overtone_n sensor_ns] ]
-
-    Args:
-        time (np.Array): formatted time values from raw .qsd
-        freq (np.Array): formatted frequency values from raw .qsd
-        dis (np.Array): formatted dissipation values from raw .qsd
-        reslen (int): reslen is num of total entries (ns * n overtones)
-        ns (int): num sensors
-
-    Returns:
-        _type_: _description_
-    """    
     df = pd.DataFrame()
     reslen = len(reslen)
     n_overtones = int(reslen / ns)
 
-    # ensure lengths are consistent (time and data may be offset by 1)
+    # a file with only a single overtone never gets stacked into a 2D
+    # array by read_qsd, so normalize to 2D here for consistent indexing
+    time = np.atleast_2d(time)
+    freq = np.atleast_2d(freq)
+    dis = np.atleast_2d(dis)
+
     if len(time[0]) < len(freq[0]) and len(time[0]) < len(dis[0]):
         global_time = np.insert(time[0], 0, 0)
     else:
         global_time = time[0]    
     df["Time"] = global_time
 
-    # devices use either 1 or 4 sensors, use only 1 sensor
     for i in range(n_overtones):
         cur_overtone = 'fundamental' if i == 0 else ordinal(i * 2 + 1)
         df[f"{cur_overtone}_freq"] = freq[i]
         df[f"{cur_overtone}_dis"] = dis[i]
 
-    # remove 0 entries
     df = df.loc[(df >= 1e-8).all(axis=1)]
-
-    print(df.head)    
     return df
-
-if __name__ == '__main__':
-    [tim,fre,dis,reslen,ns]=read_qsd("raw_data/qsense_bsa/BSA.1mgml-1.280723.qsd")
-
-    # for demo only: remove for a "useful" application
-    plt.subplot(211)
-    for k in range(0,min(np.shape(fre))):
-        plt.plot(tim[k,:reslen[k]],fre[k,:reslen[k]]-fre[k][0])
-    plt.ylabel('freq. variation (Hz)')
-    plt.subplot(212)
-    for k in range(0,min(np.shape(dis))):
-        plt.plot(tim[k,:reslen[k]],dis[k,:reslen[k]]-dis[k][0])
-    plt.xlabel('time (s)')
-    plt.ylabel('dissipation (no unit)')
-    plt.show()
